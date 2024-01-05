6c6847db57c56bcdb40be0144511c6335cfc22da
3elk4 <izaa1109@gmail.com>
2024-01-05 15:31:07 +0100

    create empty sudoku cell class

---
+++ SudokuSolver/SudokuSolver/EmptySudokuCell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    public class EmptySudokuCell
+    {
+        public int Row { get; init; }
+        public int Col { get; init; }
+
+        public List<int> PossibleValues { get; init; } = new();
+        public EmptySudokuCell(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+    }
+}
