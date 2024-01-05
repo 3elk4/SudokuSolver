47e27155092c5866dd21d9c4caa308d316093f25
3elk4 <izaa1109@gmail.com>
2024-01-05 15:30:44 +0100

    create result class

---
+++ SudokuSolver/SudokuSolver/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    public class Result
+    {
+        public bool IsSolved { get; init; }
+        public Sudoku Sudoku { get; init; }
+
+        public Result(bool isSolved, Sudoku sudoku)
+        {
+            IsSolved = isSolved;
+            Sudoku = sudoku;
+        }
+    }
+}
