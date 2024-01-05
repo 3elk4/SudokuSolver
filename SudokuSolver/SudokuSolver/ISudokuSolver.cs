88d1add56c5741f1d740d6e4439f7e90c8d9e522
3elk4 <izaa1109@gmail.com>
2024-01-05 15:31:47 +0100

    create interface for another implementation for sudoku solver

---
+++ SudokuSolver/SudokuSolver/ISudokuSolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    interface ISudokuSolver
+    {
+        public Result Solve(Sudoku sudoku);
+    }
+}
